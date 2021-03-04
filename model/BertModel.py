62ab6cb9a1d3d995a540c8f9c19bf407b6dbe00b
ZhangYunchenY <yunchenx@outlook.com>
2021-03-04 16:29:47 +0800

    update

--- model/BertModel.py
+++ model/BertModel.py
@@ -4,22 +4,33 @@ from transformers import BertModel
 
 
 class BertMRCBIO(nn.Module):
-    def __init__(self, config, token_mask, bio_num=3):
+    def __init__(self, config, head_mask, head_word_mask=None, bio_num=3):
         super(BertMRCBIO, self).__init__()
         self.bio_num = bio_num
-        self.token_mask = token_mask
-        self.bert = BertModel.from_pretrained('bert-base-chinese', config=config)
+        self.head_mask = head_mask
+        self.head_word_mask = head_word_mask
+        self.bert = BertModel.from_pretrained('bert-base-chinese', config=config, add_pooling_layer=False)
         self.hidden_size = config.hidden_size
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
-        self.cls_classifier = nn.Linear(self.hidden_size, 2)
-        self.bio_classifier = nn.Linear(self.hidden_size, self.bio_num)
+        self.bio_classifier = nn.Linear(self.hidden_size, self.bio_num)  # 用于BIO标注
 
     def forward(self, input_ids, token_type_ids=None, attention_mask=None,
-                labels=None):
+                labels=None, head_mask=None):
         outputs = self.bert(
             input_ids,
             attention_mask=attention_mask,
             token_type_ids=token_type_ids,
             return_dict=None
         )
-        pass
+        sequence_output = outputs[0]
+        sequence_without_head = torch.matmul(sequence_output, head_mask)
+        token_logits = self.bio_classifier(sequence_without_head)
+        if labels is not None:
+            labels = labels.float()
+            loss_function = nn.CrossEntropyLoss()
+            loss = loss_function(token_logits, labels)
+            return loss
+        else:
+            softmax_function = nn.Softmax(dim=1)
+            logits = softmax_function(token_logits)
+            return logits
